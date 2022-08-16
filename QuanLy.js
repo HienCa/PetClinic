d1f6084583173cbd89808da9e487fabe7920f804
Hiền Ca <nguyenvanhien050601@gmail.com>
2022-08-16 21:02:04 +0700

    ....

--- QuanLy.js
+++ QuanLy.js
@@ -1,17 +1,40 @@
 var addButton = document.getElementById("btn");
 var addForm = document.getElementById("add-product");
-var addProduct = document.getElementById("sub-btn")
-addButton.addEventListener("click", function(){
-    addForm.classList.add("active");
-})
+var addProduct = document.getElementById("add-btn");
 
-addProduct.addEventListener("click", function(){
+const updateButton = document.querySelectorAll(".update-btn");
+const updateForm = document.getElementById("up-product");
+const updateProduct = document.getElementById("update-btn");
+
+addButton.addEventListener("click", function (e) {
+    addForm.classList.add("active");
+
+});
+
+addProduct.addEventListener("click", function (e) {
+
     addForm.classList.remove("active");
-})
+
+});
+
+
+for (let i = 0; i < updateButton.length; i++) {
+    updateButton[i].addEventListener("click", function (e) {
+
+        updateForm.classList.add("active");
+
+    });
+
+}
+updateForm.addEventListener("click", function () {
+    e.stopPropagation();
+    updateForm.classList.remove("active");
+
+});
 //quanlyhoso
 var dateToday = function () {
     var myDate = document.getElementById("getDate");
     var today = new Date();
     myDate.value = today.toISOString().substring(0, 10);
- }
- dateToday();
+}
+dateToday();
